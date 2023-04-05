8ff36aa74615830f41ae99de0f1e17ca4845d4d5
Robert_Kamrowski <robert.kamrowski1999@gmail.com>
2023-04-05 22:57:27 +0200

    Add patch and getSingle progress controller methods

--- backend/routes/progressRoutes.js
+++ backend/routes/progressRoutes.js
@@ -4,13 +4,17 @@ const { authProtect } = require('../middlewares/authMiddleware')
 const {
    getProgress,
    postProgress,
+   getSingleProgress,
    getProgressChart,
+   updateProgress,
    deleteProgress
 } = require('../controllers/progressController')
 
 router.post('/progress', authProtect, postProgress)
 router.get('/progress', authProtect, getProgress)
 router.get('/progress-chart', authProtect, getProgressChart)
+router.get('/progress/:id', authProtect, getSingleProgress)
+router.patch('/progress/:id', authProtect, updateProgress)
 router.delete('/progress/:id', authProtect, deleteProgress)
 
 module.exports = router
